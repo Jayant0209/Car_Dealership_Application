49989b22eb78d9bd2862f5dbacc0112c4952175c
Jayant Agrawal <94778719+Jayant0209@users.noreply.github.com>
2024-03-17 20:54:15 +0530

    Add files via upload

---
+++ src/main/java/configuration/HibernateUtil.java
@@ -0,0 +1,20 @@
+package configuration;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
